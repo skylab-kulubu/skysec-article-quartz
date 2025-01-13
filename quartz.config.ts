00de74bba1327f5ca64184cd666ba9ea4b0a120f
farukerdem <farukomererdem2003@gmail.com>
2025-01-13 19:18:22 +0300

    light theme

--- quartz.config.ts
+++ quartz.config.ts
@@ -30,15 +30,15 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          light: "#fffff",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#fff23688",
+          dark: "#1B4965",
+          secondary: "#5FA8D3",
+          tertiary: "#62B6CB",
+          highlight: "#CAE9FF",
+          textHighlight: "#BEE9E8",
         },
         darkMode: {
           light: "#010b00",

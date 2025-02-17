7a5fc8e6106f86fad8e49bc7d83abc5f8ffcd28c
Faruk <farukomererdem2003@gmail.com>
2025-02-17 20:44:16 +0300

    baseurl modified

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "skysec.netlify.app",
+    baseUrl: "skysec.yildizskylab.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,

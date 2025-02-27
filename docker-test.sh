49d61142253a43c4832776b4d6f0480af1c31957
Faruk <farukomererdem2003@gmail.com>
2025-02-27 15:07:13 +0300

    port parameter

--- docker-test.sh
+++ docker-test.sh
@@ -3,6 +3,7 @@
 IMAGE_NAME=${IMAGE_NAME:-"skysec-article"}
 AUTHOR_NAME=${AUTHOR_NAME:-"lomarkomar"}
 VERSION=${VERSION:-"latest"}
+PORT=${PORT:-"8080"}
 docker image prune -f
 docker build -t $AUTHOR_NAME/$IMAGE_NAME:$VERSION .
-docker run --rm -itp 8080:8080 $AUTHOR_NAME/$IMAGE_NAME:$VERSION
+docker run --rm -itp $PORT:8080 $AUTHOR_NAME/$IMAGE_NAME:$VERSION
